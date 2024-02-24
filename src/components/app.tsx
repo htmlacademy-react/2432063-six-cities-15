902047fc26acfe785b9856619574bde931ba9d82
EgorGER <QwertiQQQ@gmail.com>
2024-02-24 12:14:06 +0300

    создает приватный маршрут для Favorites

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,6 @@
 import { Route, BrowserRouter, Routes} from 'react-router-dom';
+import PrivateRoute from './private-route';
+import { AuthorizationStatus } from '../const';
 
 import MainPage from '../pages/main-page/main-page';
 import LoginScreen from '../pages/login/login';
@@ -16,8 +18,13 @@ function App({quantityPlaceCard}: AppProps): JSX.Element {
       <Routes>
         <Route path='/' element={<MainPage quantityPlaceCard={quantityPlaceCard}/>} />
         <Route path='login' element={<LoginScreen />} />
-        <Route path='favorites' element={<FavoritesScreen />} />
-        <Route path='Offer' element={<Offer />} />
+        <Route path='favorites' element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <FavoritesScreen />
+          </PrivateRoute>
+        }
+        />
+        <Route path='Offer/:id' element={<Offer />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
