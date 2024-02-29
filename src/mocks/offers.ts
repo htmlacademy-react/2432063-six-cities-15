04cb11e28f021f5685aad96f55cafb140a61b518
EgorGER <QwertiQQQ@gmail.com>
2024-02-29 18:56:27 +0300

    module4-task1

---
+++ src/mocks/offers.ts
@@ -0,0 +1,79 @@
+export const offers = [
+  {
+    id: 0,
+    cardPrice: 180,
+    cardImg: 'img/apartment-02.jpg',
+    isPremium: false,
+    cardBookmarks: 'place-card__bookmark-button place-card__bookmark-button--active button',
+    cardRating: 4.5,
+    cardDescription: 'Beautiful & luxurious apartment at great location',
+    cardType: 'Apartment'
+  },
+  {
+    id: 1,
+    cardPrice: 240,
+    cardImg: 'img/room.jpg',
+    isPremium: true,
+    cardBookmarks: 'place-card__bookmark-button button',
+    cardRating: 3,
+    cardDescription: 'Beautiful & luxurious apartment at great location',
+    cardType: 'Apartment'
+  },
+  {
+    id: 2,
+    cardPrice: 190,
+    cardImg: 'img/apartment-02.jpg',
+    isPremium: false,
+    cardBookmarks: 'place-card__bookmark-button button',
+    cardRating: 4,
+    cardDescription: 'Canal View Prinsengracht',
+    cardType: 'Room'
+  },
+  {
+    id: 3,
+    cardPrice: 370,
+    cardImg: 'img/apartment-03.jpg',
+    isPremium: false,
+    cardBookmarks: 'place-card__bookmark-button button',
+    cardRating: 3,
+    cardDescription: 'Nice, cozy, warm big bed apartment',
+    cardType: 'Apartment'
+  },
+  {
+    id: 4,
+    cardPrice: 400,
+    cardImg: 'img/room.jpg',
+    isPremium: true,
+    cardBookmarks: 'place-card__bookmark-button place-card__bookmark-button--active button',
+    cardRating: 2,
+    cardDescription: 'Wood and stone place',
+    cardType: 'Room'
+  },
+];
+
+
+export const Favorite = [
+  {
+    id: '6af6f711-c28d-4121-82cd-e0b462a27f00',
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment',
+    price: 120,
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.35514938496378,
+        ongitude: 4.673877537499948,
+        zoom: 8
+      }
+    },
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 4,
+    previewImage: 'https://url-to-image/image.png'
+  }
+];
---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export type OfferType = {
+  id: number;
+  cardPrice: number;
+  cardImg: string;
+  isPremium: boolean;
+  cardBookmarks: string;
+  cardRating: number;
+  cardDescription: string;
+  cardType: string;
+};
