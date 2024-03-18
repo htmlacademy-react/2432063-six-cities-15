f27cdde7e894328de11a006c1037cca40abaede1
EgorGER <QwertiQQQ@gmail.com>
2024-03-18 11:43:17 +0300

    добовляет отоброжение "предложения рядом" на старничку оффер

---
+++ src/components/near-places-card-list.tsx
@@ -0,0 +1,23 @@
+import PlaceCard from './place-card';
+import { OffersType } from '../types/types';
+
+type NearPlaceCardListProps = {
+  offers: OffersType;
+  setCardHoverId: (id: string | null) => void;
+}
+
+function NearPlaceCardList({ offers, setCardHoverId }: NearPlaceCardListProps): JSX.Element {
+
+  return (
+    <section className="near-places places">
+      <h2 className="near-places__title">Other places in the neighbourhood</h2>
+      <div className="near-places__list places__list">
+        {offers.map((offer) => (
+          <PlaceCard key={offer.id} offerCard={offer} setCardHoverId={setCardHoverId} placeType={'near-places'}/>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default NearPlaceCardList;
