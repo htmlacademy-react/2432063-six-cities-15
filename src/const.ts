468ac4130c8c106de24469d071198bf265691dad
EgorGER <QwertiQQQ@gmail.com>
2024-03-22 12:42:24 +0300

    изменяет моки

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,5 @@
+import { CityList } from './types/types';
+
 export const reviewRating = {
   'fiveStars': '5',
   'fourStars': '4',
@@ -23,26 +25,23 @@ export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensi
 
 export const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
 
-export const LOCATIONS = [
-  {citi: 'Paris',
-    def: false,
-  },
-  {citi: 'Cologne',
-    def: false,
-  },
-  {citi: 'Brussels',
-    def: false,
-  },
-  {citi: 'Amsterdam',
-    def: true,
-  },
-  {citi: 'Hamburg',
-    def: false,
-  },
-  {citi: 'Dusseldorf',
-    def: false,
-  },
-  {citi: 'Zalupinsk',
-    def: false,
-  }
+
+export const CITY_LIST: CityList = {
+  Paris: 'Paris',
+  Cologne: 'Cologne',
+  Brussels: 'Brussels',
+  Amsterdam: 'Amsterdam',
+  Hamburg: 'Hamburg',
+  Dusseldorf: 'Dusseldorf',
+} as const;
+
+export const DEFAULT_CITY = CITY_LIST.Amsterdam;
+
+export const citiesList = [
+  CITY_LIST.Paris,
+  CITY_LIST.Cologne,
+  CITY_LIST.Brussels,
+  CITY_LIST.Amsterdam,
+  CITY_LIST.Hamburg,
+  CITY_LIST.Dusseldorf
 ];
